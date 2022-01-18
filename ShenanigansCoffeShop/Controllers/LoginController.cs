77c062570483421cfc98b878a5acd458eec2af92
Rafael Azriaiev <rafael.azrv@gmail.com>
2022-01-18 15:31:14 +0200

    Login Page added //not finished

---
+++ ShenanigansCoffeShop/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShenanigansCoffeShop.Models;
+
+namespace ShenanigansCoffeShop.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProcessLogin(UserModel userModel)
+        {
+
+            return View("LoginSuccess", userModel);
+        }
+    }
+}
