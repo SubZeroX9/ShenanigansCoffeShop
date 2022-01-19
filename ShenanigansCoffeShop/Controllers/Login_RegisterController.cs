16b39393e60afaa48edac9e14cfd070bfb67555c
Rafael Azriaiev <rafael.azrv@gmail.com>
2022-01-19 15:59:19 +0200

    Login/Register view and controller update

---
+++ ShenanigansCoffeShop/Controllers/Login_RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShenanigansCoffeShop.Models;
+
+namespace ShenanigansCoffeShop.Controllers
+{
+    public class Login_RegisterController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        public ActionResult ProcessLogin(UserModel userModel)
+        {
+
+            return View("Login", userModel);
+        }
+
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+
+        public ActionResult ProccessRegister()
+        {
+
+
+
+            return View("Login");
+        }
+    }
+}
--- ShenanigansCoffeShop/Models/UserModel.cs
+++ ShenanigansCoffeShop/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,25 @@ namespace ShenanigansCoffeShop.Models
 {
     public class UserModel
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$",ErrorMessage = "Password length must be minimum of 8 and maximum 16 and contain at least 1 Uppercase letter, 1 LowerCase letter, 1 Number and 1 special character")]
         public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Key]
+        [Required]
+        [RegularExpression("^[\w -\.] +@([\w -] +\.)+[\w-]{2,4}$", ErrorMessage="Please Enter a valid E-Mail")]
+        public string Email { get; set; }
+
+        public string MemberType { get; set; }
+
+        public string UserType { get; set; }
+
+        public string PhoneNum { get; set; }
+
+        public DateTime BirthDay { get; set; }
     }
 }
