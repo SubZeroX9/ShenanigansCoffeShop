537331c083a7fc4006b82be10b2a3c236ad4d54d
Rafael Azriaiev <rafael.azrv@gmail.com>
2022-01-19 14:26:03 +0200

    added bootstrap v5 + registration form

--- ShenanigansCoffeShop/Dal/UserDal.cs
+++ ShenanigansCoffeShop/Dal/UserDal.cs
@@ -12,7 +12,7 @@ namespace ShenanigansCoffeShop.Dal
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<UserModel>().ToTable(""); // need to add somthing in " "
+            modelBuilder.Entity<UserModel>().ToTable("users");
         }
         public DbSet<UserModel> Users { get; set; }
     }
