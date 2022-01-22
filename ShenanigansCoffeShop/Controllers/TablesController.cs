c4845067170ca7690350a9f659fbcd3fa96c9b40
Rafael Azriaiev <rafael.azrv@gmail.com>
2022-01-22 20:37:41 +0200

    tables added dynamic db

--- ShenanigansCoffeShop/Controllers/TablesController.cs
+++ ShenanigansCoffeShop/Controllers/TablesController.cs
@@ -19,5 +19,48 @@ namespace ShenanigansCoffeShop.Controllers
 
             return View("ChooseTable", tvm);
         }
+
+        public ActionResult ChooseTableProccess(CTableModel table)
+        {
+            if (Session["CurrentOrderObj"] == null)
+            {
+                TotalOrderDal totalOrderDal = new TotalOrderDal();
+                string NewOrderNum = (Convert.ToInt64(totalOrderDal.TotalOrderList.Max(x => x.o_num)) + 1).ToString();
+                TotalOrderModel newTotalOrder = new TotalOrderModel();
+                newTotalOrder.o_num = NewOrderNum;
+                newTotalOrder.email_addr = ((UserModel)Session["CurrentUserObj"]).email_addr;
+                newTotalOrder.currentOrder = true;
+                DateTime newDate = DateTime.Now;
+                newTotalOrder.o_date = new DateTime(newDate.Year, newDate.Month, newDate.Day);
+                newTotalOrder.totalprice = 0;
+                newTotalOrder.t_num = table.t_num;
+                totalOrderDal.TotalOrderList.Add(newTotalOrder);
+                totalOrderDal.SaveChanges();
+                Session["CurrentOrderObj"] = newTotalOrder;
+            }
+            else
+            {
+                if (((TotalOrderModel) Session["CurrentOrderObj"]).t_num != null)
+                {
+                    CTableDal CTdal = new CTableDal();
+                    CTableModel prevTable = ctDal.Tables.Where(x => x.s_num == ((TotalOrderModel)Session["CurrentOrderObj"]).t_num).FirstOrDefault();
+                    prevTable.availability = true;
+                    CTdal.SaveChanges();
+                }
+
+                TotalOrderDal totalOrderDal = new TotalOrderDal();
+                TotalOrderModel currentOrder = totalOrderDal.TotalOrderList.Where(x => x.o_num == ((TotalOrderModel) Session["CurrentOrderObj"]).o_num).FirstOrDefault();
+                currentOrder.t_num = table.t_num;
+                totalOrderDal.SaveChanges();
+                Session["CurrentOrderObj"] = currentOrder;
+            }
+
+            CTableDal ctDal = new CTableDal();
+            CTableModel tableModel = ctDal.Tables.Where(x => x.s_num == table.t_num).FirstOrDefault();
+            tableModel.availability = false;
+            ctDal.SaveChanges();
+
+            return View("Index");
+        }
     }
 }
